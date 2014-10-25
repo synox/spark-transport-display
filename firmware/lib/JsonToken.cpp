f99afb4b01be263c621a5cdfb1e9a1f8367374cf
Synox <synox@synox>
2014-10-25 23:45:39 +0200

    update dependencies

--- firmware/lib/JsonToken.cpp
+++ firmware/lib/JsonToken.cpp
@@ -7,10 +7,54 @@
 
 using namespace ArduinoJson::Parser;
 
+char* JsonToken::getText()
+{
+    char* s = _json + _token->start;
+    _json[_token->end] = 0;
+
+    unescapeString(s);
+
+    return s;
+}
+
+inline void JsonToken::unescapeString(char* s)
+{
+    char* readPtr = s;
+    char* writePtr = s;
+    char c;
+
+    do
+    {
+        c = *readPtr++;
+
+        if (c == '\\')
+        {
+            c = unescapeChar(*readPtr++);
+        }
+
+        *writePtr++ = c;
+
+    } while (c != 0);
+}
+
+inline char JsonToken::unescapeChar(char c)
+{
+    // Optimized for code size on a 8-bit AVR
+
+    const char* p = "b\bf\fn\nr\rt\t";
+
+    while (true)
+    {
+        if (p[0] == 0) return c;
+        if (p[0] == c) return p[1];
+        p += 2;
+    }
+}
+
 JsonToken JsonToken::nextSibling() const
 {
     // start with current token
-    jsmntok_t* t = token;
+    jsmntok_t* t = _token;
 
     // count the number of token to skip
     int yetToVisit = 1;
@@ -23,5 +67,5 @@ JsonToken JsonToken::nextSibling() const
     }
 
     // build a JsonToken at the new location
-    return JsonToken(json, t);
+    return JsonToken(_json, t);
 }
--- firmware/lib/JsonToken.h
+++ firmware/lib/JsonToken.h
@@ -18,33 +18,29 @@ namespace ArduinoJson
 
             // Create a "null" pointer
             JsonToken()
-                : token(0)
+                : _token(0)
             {
             }
 
             // Create a pointer to the specified JSON token
             JsonToken(char* json, jsmntok_t* token)
-                : json(json), token(token)
+                : _json(json), _token(token)
             {
             }
 
             // Get content of the JSON token
-            char* getText()
-            {
-                json[token->end] = 0;
-                return json + token->start;
-            }
+            char* getText();
 
             // Get the number of children tokens
             int childrenCount()
             {
-                return token->size;
+                return _token->size;
             }
 
             // Get a pointer to the first child of the current token
             JsonToken firstChild() const
             {
-                return JsonToken(json, token + 1);
+                return JsonToken(_json, _token + 1);
             }
 
             // Get a pointer to the next sibling token (ie skiping the children tokens)
@@ -53,37 +49,37 @@ namespace ArduinoJson
             // Test equality
             bool operator!=(const JsonToken& other) const
             {
-                return token != other.token;
+                return _token != other._token;
             }
 
             // Tell if the pointer is "null"
             bool isValid()
             {
-                return token != 0;
+                return _token != 0;
             }
 
             // Tell if the JSON token is a JSON object
             bool isObject()
             {
-                return token != 0 && token->type == JSMN_OBJECT;
+                return _token != 0 && _token->type == JSMN_OBJECT;
             }
 
             // Tell if the JSON token is a JSON array
             bool isArray()
             {
-                return token != 0 && token->type == JSMN_ARRAY;
+                return _token != 0 && _token->type == JSMN_ARRAY;
             }
 
             // Tell if the JSON token is a primitive
             bool isPrimitive()
             {
-                return token != 0 && token->type == JSMN_PRIMITIVE;
+                return _token != 0 && _token->type == JSMN_PRIMITIVE;
             }
 
             // Tell if the JSON token is a string
             bool isString()
             {
-                return token != 0 && token->type == JSMN_STRING;
+                return _token != 0 && _token->type == JSMN_STRING;
             }
 
             // Explicit wait to create a "null" JsonToken
@@ -93,8 +89,11 @@ namespace ArduinoJson
             }
 
         private:
-            char* json;
-            jsmntok_t* token;
+            char* _json;
+            jsmntok_t* _token;
+            
+            static char unescapeChar(char c);
+            static void unescapeString(char* s);
         };
     }
 }
